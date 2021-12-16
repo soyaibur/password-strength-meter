737e9dde1e8fb9294f931c520135cf7f84113690
soyaibur <ovisoyaiburrahman@gmail.com>
2021-12-17 05:28:56 +0600

    default files to start a new project

---
+++ logic.js
@@ -0,0 +1,28 @@
+// ....Univesal Stracture of function and others......
+// ::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+
+
+// .....All Element Selection will be undernith here...........
+// ::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+
+
+// .....All EventListener will go undernith here...........
+// ::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+
+
+// ......All Function will go here undernith..........
+// ::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+//All code will be deleted here.............
+function printOut(text){
+    const div = document.createElement('div')
+    div.innerHTML = text
+    document.body.appendChild(div)
+}
+// addEventListener('click',()=>{
+//     alert("this is working")
+// })
+// ::::::::::::::::::::::::::::::::::::::::::::::::::::::::
